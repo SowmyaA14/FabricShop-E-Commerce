12bc906ce1e18a6513bab10096268ad12b56f77b
Sowmya A <115523988+SowmyaA14@users.noreply.github.com>
2025-03-17 22:17:46 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: "Ajrak Screen Printed Fabric",
+    image: "/images/Ajrak.jpg",
+    description:
+      "Buttery Silken Ajrak Screen Printed Fabrics in vivid shades to brighten up your day. Modal Silk with the shine and flow are just right for any silhouette !",
+    brand: "Nali",
+    category: "Silk Fabric",
+    price: 600.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Kantha's Handloomed Cotton Fabric",
+    image: "/images/Fabric-2.jpg",
+    description:
+      "Hand Block Printed Dabu Natural Indigo Floral Motifs 100% Cotton Fabric Design 652",
+    brand: "Kantha",
+    category: "Cotton Fabric",
+    price: 500.0,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Textile pattern design by Chennai Silks",
+    image: "/images/Fabric-3.jpg",
+    description:
+      "Aesthetic and intricately crafted textile patterns blending tradition with modern elegance for unique fabric designs.",
+    brand: "Chennai Silks",
+    category: "Pattern Design Fabrics",
+    price: 400.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Kantha's Flower Printed Cotton Fabric",
+    image: "/images/Fabric-4.jpg",
+    description:
+      "Indian Block Print Cotton Fabric, 100% Cotton Fabric, Flower Print Fabric, Sewing Quilting Crafting Fabric, Fabric For India, Home Decor",
+    brand: "Kantha",
+    category: "Cotton Fabric",
+    price: 500.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: "Black Banarasi Silk Fabric",
+    image: "/images/Fabric-5.jpg",
+    description:
+      "Black Banarasi Indian Brocade Fabric by the Yard Blended Silk Bridal",
+    brand: "Etsy",
+    category: "Banarasi Silk Fabric",
+    price: 700.0,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Hand loomed Kanchipuram silk fabric",
+    image: "/images/Fabric-6.jpg",
+    description:
+      "Featuring a stunning Kanchipuram silk saree that beautifully combines elegance and tradition.",
+    brand: "Nali",
+    category: "Kanchipuram Silk Sarees",
+    price: 800.0,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
